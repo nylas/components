eb9b8d546b09525a4877d6b61e53d1b6f0e79c7b
Heather Hamilton <55773810+heathergh@users.noreply.github.com>
2021-08-04 16:38:26 -0400

    Nylas-Error component styling and restriction to localhost (#23)

--- commons/src/components/NError.svelte
+++ commons/src/components/NError.svelte
@@ -7,15 +7,23 @@
   export let id; // component id
 
   $: error = $ErrorStore[id] || { name: "" };
-  $: explanation = NErrorExplanationMap[error.name]();
+  $: explanation = NErrorExplanationMap[errorName]();
+  $: errorName = !error.name ? error.message?.name : error.name;
+  const isDevEnv = window.location.href.includes("localhost");
 </script>
 
 <style>
   .message-container {
-    padding: 0.5rem;
-    font-size: 11px;
-    color: red;
-    text-align: initial;
+    background: #fff6f6;
+    border-radius: 5px;
+    box-shadow: 0 0 0 1px #aa92a0 inset, 0 0 0 0 transparent;
+    color: #9f3a38;
+    font-size: 1.25rem;
+    padding: 10px;
+    margin: 0 auto;
+    transition: opacity 500ms ease, color 500ms ease,
+      background-color 500ms ease, box-shadow 500ms ease,
+      -webkit-box-shadow 500ms ease;
   }
 
   .message-container *:focus {
@@ -24,22 +32,28 @@
   }
 
   .details {
-    font-size: 8px;
     color: #494949;
+    font-size: 0.75rem;
+    width: 100%;
   }
 </style>
 
-{#if error.name}
+{#if errorName && isDevEnv}
   <div class="message-container">
-    <h3>
-      {@html explanation.title}
-    </h3>
-    <h4>
-      {@html explanation.subtitle}
-    </h4>
+    {#if explanation.title}
+      <h3>
+        {@html explanation.title}
+      </h3>
+    {/if}
+    {#if explanation.subtitle}
+      <h4>
+        {@html explanation.subtitle}
+      </h4>
+    {/if}
     <span class="details">Debug info:</span>
-    <textarea class="details" readonly cols={64}>
-      {error.name}: {id}
+    <textarea class="details" readonly>
+      {errorName}: {id}
+      {error.message.message ? error.message.message : ""}
     </textarea>
   </div>
 {/if}
