211cec0ff78b05fd4b13b12a3b5fc17209025e24
Oliver Zsivanov <ozsivanov@gmail.com>
2021-09-08 14:52:25 -0400

    Unwrapped availability component from scheduler

--- commons/src/types/Scheduler.ts
+++ commons/src/types/Scheduler.ts
@@ -1,8 +1,12 @@
-import type { Manifest as AvailabilityManifest } from "@commons/types/Availability";
+import type {
+  Manifest as AvailabilityManifest,
+  TimeSlot,
+} from "@commons/types/Availability";
 
 export interface Manifest extends AvailabilityManifest {
   availability_id?: string;
   booking_label?: string;
   event_title?: string;
   event_description?: string;
+  slots_to_book?: TimeSlot[];
 }
