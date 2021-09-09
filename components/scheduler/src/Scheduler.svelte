58634bfd76a62ef7b4ac42ff10791c138ac3182b
Phil Renaud <phil@riotindustries.com>
2021-09-09 01:11:45 -0400

    Prototype: scheduler reading from schedule editor manifest

--- components/scheduler/src/Scheduler.svelte
+++ components/scheduler/src/Scheduler.svelte
@@ -1,6 +1,12 @@
 <svelte:options tag="nylas-scheduler" />
 
 <script lang="ts">
+  import {
+    ManifestStore,
+    EventStore,
+    AvailabilityStore,
+    fetchManifest,
+  } from "../../../commons/src";
   import { createEvent } from "@commons/connections/events";
   import { get_current_component } from "svelte/internal";
   import {
@@ -8,17 +14,19 @@
     getEventDispatcher,
     getPropertyValue,
   } from "@commons/methods/component";
+
+  import type { Manifest as EditorManifest } from "@commons/types/ScheduleEditor";
+  import type { Manifest } from "@commons/types/Scheduler";
   import type { TimeSlot } from "@commons/types/Availability";
   import type { EventQuery, TimespanEvent } from "@commons/types/Events";
-  import type { Manifest } from "@commons/types/Scheduler";
   import { onMount, tick } from "svelte";
-  import { AvailabilityStore, ManifestStore } from "../../../commons/src";
   import "../../availability/src/Availability.svelte";
 
   // #region props
   export let id: string = "";
   export let access_token: string = "";
   export let availability_id: string;
+  export let editor_id: string;
   export let email_ids: string[];
   export let booking_label: string;
   export let event_title: string;
@@ -29,13 +37,21 @@
   //#region mount and prop initialization
   let internalProps: Partial<Manifest> = {};
   let manifest: Partial<Manifest> = {};
+  let editorManifest: Partial<EditorManifest> = {};
+
   onMount(async () => {
     await tick();
     const storeKey = JSON.stringify({ component_id: id, access_token });
     manifest = (await $ManifestStore[storeKey]) || {};
 
+    if (editor_id) {
+      editorManifest = await fetchManifest(editor_id);
+    }
+
     internalProps = buildInternalProps($$props, manifest) as Partial<Manifest>;
   });
+
+  $: console.log({ editorManifest });
 
   $: {
     const rebuiltProps = buildInternalProps(
@@ -53,6 +69,7 @@
       availability_id,
       "",
     );
+    editor_id = getPropertyValue(internalProps.editor_id, editor_id, "");
     email_ids = getPropertyValue(internalProps.email_ids, email_ids, []);
     booking_label = getPropertyValue(
       internalProps.booking_label,
@@ -60,7 +77,7 @@
       "Schedule",
     );
     event_title = getPropertyValue(
-      internalProps.event_title,
+      internalProps.event_title || editorManifest.event_title,
       event_title,
       "Meeting",
     );
@@ -123,6 +140,7 @@
       <ul>
         {#each slots_to_book as timeSlot}
           <li>
+            <h3>{event_title}: {event_description}</h3>
             {timeSlot.start_time.toLocaleString()} to {timeSlot.end_time.toLocaleString()}
           </li>
         {/each}
