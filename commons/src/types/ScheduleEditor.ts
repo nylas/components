4ae7fd4f1571c9d626c9eb79b65e6ef69095b817
Phil Renaud <phil@riotindustries.com>
2021-09-15 22:40:41 -0400

    Adds new fields to editor

--- commons/src/types/ScheduleEditor.ts
+++ commons/src/types/ScheduleEditor.ts
@@ -4,4 +4,6 @@ export interface Manifest extends NylasManifest {
   event_title?: string;
   event_description?: string;
   show_hosts?: "show" | "hide";
+  event_location?: string;
+  event_conferencing?: string;
 }
