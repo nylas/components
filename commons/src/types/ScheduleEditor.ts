943273b4ff3feda51769f862797fcafb4c2e8b8b
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2021-09-28 05:40:18 +0530

    --WIP

--- commons/src/types/ScheduleEditor.ts
+++ commons/src/types/ScheduleEditor.ts
@@ -24,6 +24,5 @@ export interface Manifest extends NylasManifest {
   notification_message?: string;
   notification_subject?: string;
   view_as?: "schedule" | "list";
-  pre_event_buffer: number;
-  post_event_buffer: number;
+  event_buffer: number;
 }
