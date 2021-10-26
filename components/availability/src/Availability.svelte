c1c1b49a7a08b2f1f3a2a5db0ba85f96c315472b
Phil Renaud <phil@riotindustries.com>
2021-10-26 00:33:21 -0400

    slotRef as a multidim array

--- components/availability/src/Availability.svelte
+++ components/availability/src/Availability.svelte
@@ -134,15 +134,16 @@
         );
       }
 
-      $AvailabilityStore[JSON.stringify(availabilityQuery)] =
-        $AvailabilityStore[JSON.stringify(availabilityQuery)].then(
-          (availability) => {
-            for (const calendar of availability) {
-              calendar.time_slots.push(...selectedSlots);
-            }
-            return availability;
-          },
-        );
+      $AvailabilityStore[
+        JSON.stringify(availabilityQuery)
+      ] = $AvailabilityStore[JSON.stringify(availabilityQuery)].then(
+        (availability) => {
+          for (const calendar of availability) {
+            calendar.time_slots.push(...selectedSlots);
+          }
+          return availability;
+        },
+      );
 
       await getAvailability();
     }
@@ -166,11 +167,14 @@
   let manifest: Partial<Manifest> = {};
   let loading: boolean;
   let dayRef: HTMLElement[] = [];
-  let slotRef: HTMLElement[] = [];
+  let slotRef: HTMLElement[][] = [[]];
+  $: slotRef = Array(datesToShow).fill([]);
   let slotYPositions: Record<string, DOMRect> = {};
   let shouldUpdateSlotPositions = false;
   let dayXPositions: Record<string, DOMRect> = {};
   let shouldUpdateDayPositions = false;
+
+  $: console.log({ slotRef });
 
   $: {
     if (dates_to_show || show_ticks || show_as_week || show_weekends) {
@@ -189,7 +193,7 @@
       allow_date_change
     ) {
       // Changes to these props changes the height of our slot buttons
-      slotRef = slotRef.filter((slot) => !!slot);
+      slotRef = slotRef.filter((slot) => !!slot); // TEMP TODO
       shouldUpdateSlotPositions = true;
     }
   }
@@ -230,7 +234,7 @@
 
   afterUpdate(() => {
     if (shouldUpdateSlotPositions) {
-      slotYPositions = recalibratePositions(slotRef);
+      slotYPositions = recalibratePositions(slotRef[0]);
       shouldUpdateSlotPositions = false;
     }
     if (shouldUpdateDayPositions) {
@@ -908,7 +912,9 @@
               hour12: true,
             });
       return `
-      ${startTime.replace(" AM", "am").replace(" PM", "pm")} - ${endTime
+      ${startTime
+        .replace(" AM", "am")
+        .replace(" PM", "pm")} - ${endTime
         .replace(" AM", "am")
         .replace(" PM", "pm")}
       `;
@@ -1310,8 +1316,10 @@
       event.touches.length === 1 &&
       event.changedTouches.length === 1 // check if there is a single touch point
     ) {
-      const { pageX: touchPositionX, pageY: touchPositionY } =
-        event.changedTouches[0];
+      const {
+        pageX: touchPositionX,
+        pageY: touchPositionY,
+      } = event.changedTouches[0];
 
       const currentTouchedDayPosition = Object.entries(dayXPositions).find(
         ([_, dayPosition]) => dayPosition.x > touchPositionX,
@@ -1339,6 +1347,19 @@
   }
 
   const throttledTouchMovement = throttle(handleTouchMovement, 100);
+
+  function arrowNavigate(code: string, slotIndex: number, dayIndex: number) {
+    console.log("slotIndex", slotIndex, slotRef);
+    if (code === "ArrowDown") {
+      slotRef[dayIndex][slotIndex + 1]?.focus();
+    } else if (code === "ArrowUp") {
+      slotRef[dayIndex][slotIndex - 1]?.focus();
+    } else if (code === "ArrowLeft") {
+      slotRef[dayIndex - 1][slotIndex]?.focus();
+    } else if (code === "ArrowRight") {
+      slotRef[dayIndex + 1][slotIndex]?.focus();
+    }
+  }
   //#endregion slot interaction handlers
 
   // #region error
@@ -1384,9 +1405,18 @@
   class:hide-header={!show_header}
   on:mouseleave={() => endDrag(null)}
   style="
-  --busy-color-lightened: {lightenHexColour(busy_color, 90)};
-  --closed-color-lightened: {lightenHexColour(closed_color, 90)};
-  --selected-color-lightened: {lightenHexColour(selected_color, 60)}; 
+  --busy-color-lightened: {lightenHexColour(
+    busy_color,
+    90,
+  )};
+  --closed-color-lightened: {lightenHexColour(
+    closed_color,
+    90,
+  )};
+  --selected-color-lightened: {lightenHexColour(
+    selected_color,
+    60,
+  )}; 
 --free-color: {free_color}; --busy-color: {busy_color}; --closed-color: {closed_color}; --partial-color: {partial_color}; --selected-color: {selected_color};"
 >
   <header class:dated={allow_date_change}>
@@ -1520,7 +1550,7 @@
                 ).toLocaleString()} to {new Date(
                   slot.end_time,
                 ).toLocaleString()}; Free calendars: {slot.available_calendars.toString()}"
-                bind:this={slotRef[slotIndex]}
+                bind:this={slotRef[dayIndex][slotIndex]}
                 class="slot {slot.selectionStatus} {slot.availability}"
                 class:pending={slot.selectionPending}
                 class:hovering={slot.hovering}
@@ -1544,6 +1574,11 @@
                     tick().then(() => endDrag(day));
                   }
                 }}
+                on:keydown={(e) => {
+                  if (e.code.startsWith("Arrow")) {
+                    arrowNavigate(e.code, slotIndex, dayIndex);
+                  }
+                }}
                 on:touchstart={(event) => {
                   const isFirstTouch =
                     event.touches.length === 1 &&
@@ -1556,8 +1591,10 @@
                 }}
                 on:touchmove={throttledTouchMovement}
                 on:touchend={(event) => {
-                  const { pageX: touchPositionX, pageY: touchPositionY } =
-                    event.changedTouches[0];
+                  const {
+                    pageX: touchPositionX,
+                    pageY: touchPositionY,
+                  } = event.changedTouches[0];
 
                   const allSlotPositions = Object.values(slotYPositions);
                   const top = Math.floor(allSlotPositions.splice(0, 1)[0].top);
