28880c1892bb3d74d23d0dc5d860bfac0480107c
Arjun Kalburgi <askalburgi@gmail.com>
2021-10-28 15:25:32 -0400

    [Availability] abstract date strings (#152)
    * [Availability] abstract date strings
    
    * update with getMonthString
    
    * Pr comments

--- commons/src/methods/datetime.ts
+++ commons/src/methods/datetime.ts
@@ -1,7 +1,60 @@
+import type { Day } from "@commons/types/Availability";
+
+/**
+ * Eventually, perhaps a design-system-level guideline for date and time strings.
+ *
+ * Currently, Date logic: https://www.figma.com/file/oiCKNsHDfAo9KnH1Sbs8Xj/Email-%26-Mailbox-Component?node-id=128%3A51
+ * No time logic standard yet
+ */
+
+export function showDateRange(days: Day[]): string {
+  const firstDay = days[0].timestamp;
+  const lastDay = days[days.length - 1].timestamp;
+
+  if (lastDay.getMonth() !== firstDay.getMonth()) {
+    if (lastDay.getFullYear() !== firstDay.getFullYear()) {
+      // Case 1: months and years differ: Dec 2021 - Jan 2022
+      return `${getMonthString(firstDay, true)} - ${getMonthString(
+        lastDay,
+        true,
+      )}`;
+    }
+    // Case 2: months differ, years the same: Oct - Nov 2021
+    return `${getMonthString(firstDay)} - ${getMonthString(lastDay, true)}`;
+  }
+  // Case 3: months, and therefore years, are the same: Oct 2021
+  return getMonthString(firstDay, true);
+}
+
+export function getCondensedTimeString(date: Date): string {
+  // 1 am | 1:15 am
+  const timeString = date.toLocaleTimeString([], { timeStyle: "short" });
+  if (date.getMinutes() === 0) {
+    return date
+      .toLocaleTimeString([], {
+        hour: "numeric",
+        hour12: true,
+      })
+      .replaceAll(/\./g, "");
+  }
+  return timeString.replaceAll(/\./g, "");
+}
+
 export function getTimeString(date: Date): string {
-  // 11:11am
+  // 11:11 am, Noon
+  const timeString = date.toLocaleTimeString([], { timeStyle: "short" });
+  if (timeString === "12:00 p.m.") {
+    return "Noon";
+  }
+  return timeString.replaceAll(/\./g, "");
+}
+
+export function getMonthString(date: Date, withYear = false): string {
   return date
-    .toLocaleTimeString([], { timeStyle: "short" })
+    .toLocaleDateString("default", {
+      month: "short",
+      year: withYear ? "numeric" : undefined,
+    })
     .replaceAll(/\./g, "");
 }
 
@@ -30,7 +83,6 @@ export function getDateString(date: Date): string {
     .replaceAll(/[.]/g, "");
 }
 
-// Date logic: https://www.figma.com/file/oiCKNsHDfAo9KnH1Sbs8Xj/Email-%26-Mailbox-Component?node-id=128%3A51
 export function getDate(date: Date): string {
   const today = new Date();
   if (today.toDateString() === date.toDateString()) {
