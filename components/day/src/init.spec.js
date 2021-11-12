ea853dca01fbe7c5256fe79c3a7c187d666cc8c4
Oliver Zsivanov <ozsivanov@gmail.com>
2021-11-12 14:20:15 -0500

    Removed deprecated 'Day' component

--- components/day/src/init.spec.js
+++
@@ -1,7 +0,0 @@
-xdescribe("Day component", () => {
-  xit("Time is a flat circle", () => {
-    // todo: no id
-    cy.visit("/components/day/src/index.html");
-    cy.get("nylas-day").should("exist");
-  });
-});
--- components/day/src/main.ts
+++
@@ -1,4 +0,0 @@
-import "@commons/define-component-patch";
-import Day from "./Day.svelte";
-
-export default Day;
