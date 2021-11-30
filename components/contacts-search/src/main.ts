8beb3970fc7f4d8524ae1e2e1338a4be7855e922
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2021-11-30 17:18:30 -0500

    Removing main.ts for svelte components

--- components/contacts-search/src/main.ts
+++
@@ -1,4 +0,0 @@
-import "@commons/define-component-patch";
-import ContactsSearch from "./ContactsSearch.svelte";
-
-export default ContactsSearch;
--- components/datepicker/src/main.ts
+++
@@ -1,4 +0,0 @@
-import "@commons/define-component-patch";
-import Datepicker from "./Datepicker.svelte";
-
-export default Datepicker;
