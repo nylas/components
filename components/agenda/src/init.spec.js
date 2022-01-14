90a43900d1f9833371d0a6169b5f3b1362cd6231
Kris Gardiner <kris.g@nylas.com>
2022-01-14 13:34:28 -0500

    [agenda] add fixtures and intercepts to tests

--- components/agenda/src/init.spec.js
+++ components/agenda/src/init.spec.js
@@ -1,202 +1,330 @@
 const AGENDA_EVENTS = [
   {
-    title: "Some event that I am manipulating outside of the context of Nylas",
-    description: "Passed in from HTML!",
+    title: "Static Event 1",
+    description: "This event was passed in manually via events prop",
     participants: [],
     when: {
-      end_time: 1600444800,
+      end_time: 1600444800, // 12 PM
       object: "timespan",
-      start_time: 1600437600,
+      start_time: 1600437600, // 10 AM
     },
   },
   {
-    title: "Some I got from elsewhere",
-    description: "Passed in from HTML!",
+    title: "Static Event 2",
+    description: "This event was passed in manually via events prop",
     participants: [],
     when: {
-      end_time: 1600452000,
+      end_time: 1600452000, // 2 PM
       object: "timespan",
-      start_time: 1600448400,
+      start_time: 1600448400, // 1 PM
     },
   },
 ];
 
-describe("Restricting Dates", () => {
-  it("handles allowed_dates as an array of dates ", () => {
-    cy.visit("/components/agenda/src/index.html");
-    cy.get("nylas-agenda").should("exist");
-    cy.get("nylas-agenda").shadow().find("h2").should("exist");
-    cy.get("nylas-agenda").invoke("attr", "header_type", "full");
-    cy.get("nylas-agenda").invoke("attr", "allow_date_change", true);
-    cy.get("nylas-agenda").invoke("attr", "selected_date", "");
-    cy.get("nylas-agenda").invoke("attr", "allowed_dates", [
-      new Date("March 24 2021"),
-      new Date("March 29 2021"),
-      new Date("March 30 2021"),
-    ]);
+describe("Agenda minimal display", () => {
+  beforeEach(() => {
+    cy.clock(1642179039224);
 
-    cy.get("nylas-agenda").shadow().find("h2").should("exist");
-    cy.get("nylas-agenda").shadow().find("h2").should("contain", "Wednesday");
-    cy.get("nylas-agenda").shadow().find("h2").should("contain", "24");
-    cy.get("nylas-agenda")
-      .shadow()
-      .find(".day > button")
-      .eq(0)
-      .should("have.attr", "disabled");
-    cy.get("nylas-agenda")
-      .shadow()
-      .find(".day > button")
-      .eq(1)
-      .should("not.have.attr", "disabled");
-    cy.get("nylas-agenda").shadow().find(".day > button").eq(1).click();
-    cy.get("nylas-agenda").shadow().find("h2").should("contain", "Monday");
-    cy.get("nylas-agenda").shadow().find("h2").should("contain", "29");
-    cy.get("nylas-agenda")
-      .shadow()
-      .find(".day > button")
-      .eq(0)
-      .should("not.have.attr", "disabled");
-    cy.get("nylas-agenda").shadow().find(".day > button").eq(1).click();
-    cy.get("nylas-agenda")
-      .shadow()
-      .find(".day > button")
-      .eq(1)
-      .should("have.attr", "disabled");
-  });
-
-  it("handles allowed_dates as a comma-separated string ", () => {
-    cy.visit("/components/agenda/src/index.html");
-    cy.get("nylas-agenda").should("exist");
-    cy.get("nylas-agenda").shadow().find("h2").should("exist");
-    cy.get("nylas-agenda").invoke("attr", "header_type", "full");
-    cy.get("nylas-agenda").invoke("attr", "allow_date_change", true);
-    cy.get("nylas-agenda").invoke("attr", "selected_date", "");
-    cy.get("nylas-agenda").invoke(
-      "attr",
-      "allowed_dates",
-      "March 22 2021, March 28 2021",
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/manifest",
+      {
+        fixture: "agenda/manifest.json",
+      },
+    );
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/calendars/test-calendar-id",
+      {
+        fixture: "agenda/calendars/calendarId.json",
+      },
+    );
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/agenda/events?calendar_id=test-calendar-id*",
+      {
+        fixture: "agenda/events.json",
+      },
     );
 
-    cy.get("nylas-agenda").shadow().find("h2").should("exist");
-    cy.get("nylas-agenda")
-      .shadow()
-      .find(".day > button")
-      .eq(0)
-      .should("have.attr", "disabled");
-    cy.get("nylas-agenda")
-      .shadow()
-      .find(".day > button")
-      .eq(1)
-      .should("not.have.attr", "disabled");
-    cy.get("nylas-agenda").shadow().find(".day > button").eq(1).click();
-    cy.get("nylas-agenda").shadow().find("h2").should("contain", "Sunday");
-    cy.get("nylas-agenda").shadow().find("h2").should("contain", "28");
-    cy.get("nylas-agenda")
-      .shadow()
-      .find(".day > button")
-      .eq(0)
-      .should("not.have.attr", "disabled");
-    cy.get("nylas-agenda")
-      .shadow()
-      .find(".day > button")
-      .eq(1)
-      .should("have.attr", "disabled");
+    cy.visit("/components/agenda/src/cypress.html");
+    cy.get("nylas-agenda").should("exist").as("agenda");
+  });
+
+  it("displays month and year", () => {
+    cy.get("@agenda").invoke("attr", "header_type", "full");
+    cy.get("@agenda").contains("January 2022");
+  });
+
+  it("displays current day and date", () => {
+    cy.get("@agenda").invoke("attr", "header_type", "full");
+    cy.get("@agenda").contains("Friday 14");
+  });
+
+  it("displays 4 fetched events", () => {
+    cy.get("@agenda").contains("Sample Meeting 1");
+    cy.get("@agenda").contains("Sample Meeting 2");
+    cy.get("@agenda").contains("Sample Meeting 3");
+    cy.get("@agenda").contains("Sample Meeting 4");
+  });
+
+  it("displays fetched events with conference meeting details", () => {
+    cy.get("@agenda").contains(".event", "Sample Meeting 2");
+    cy.get("@agenda").contains(".event", "This is a fake zoom meeting 2");
+    cy.get("@agenda").contains(".event", "Link: fake.zoom.us");
+  });
+
+  it("displays date change buttons by default", () => {
+    cy.get("@agenda").get("button.prev").should("be.visible");
+    cy.get("@agenda").get("button.next").should("be.visible");
   });
 });
 
-describe("Custom data", () => {
-  it("Toggles between custom and Nylas data", () => {
-    cy.visit("/components/agenda/src/index.html");
-    cy.get("nylas-agenda").should("exist");
-    cy.get("nylas-agenda").then((element) => {
-      const agenda = element[0];
+describe("Agenda props", () => {
+  beforeEach(() => {
+    cy.clock(1642179039224);
 
-      // Check to make sure it's using Nylas data (My Wonderful Event is a signifier)
-      cy.get("ul.events")
-        .find("li.event h2:contains('My Wonderful Event')")
-        .should("exist")
-        .then(() => {
-          // Set custom data
-          agenda.events = AGENDA_EVENTS;
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/manifest",
+      {
+        fixture: "agenda/manifest.json",
+      },
+    );
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/calendars/test-calendar-id",
+      {
+        fixture: "agenda/calendars/calendarId.json",
+      },
+    );
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/agenda/events?calendar_id=test-calendar-id*",
+      {
+        fixture: "agenda/events.json",
+      },
+    );
 
-          // Check to make sure it's using custom data (not Nylas data)
-          cy.get("ul.events")
-            .find("li.event h2")
-            .should("not.contain", "My Wonderful Event");
-          cy.get("ul.events")
-            .find("li.event h2:contains('Some I got from elsewhere')")
-            .should("exist")
-            .then(() => {
-              // Revert custom data
-              agenda.events = null;
-
-              // Check to make sure it's using Nylas data again
-              cy.get("ul.events")
-                .find("li.event h2")
-                .should("contain", "My Wonderful Event");
-              cy.get("ul.events")
-                .find("li.event h2")
-                .should("not.contain", "Some I got from elsewhere");
-            });
-        });
-    });
+    cy.visit("/components/agenda/src/cypress.html");
+    cy.get("nylas-agenda").should("exist").as("agenda");
   });
-});
 
-describe("Timeboxing", () => {
-  it("scales midnight to midnight by default", () => {
-    cy.visit("/components/agenda/src/index.html");
-    cy.get("nylas-agenda").should("exist");
-    cy.get("nylas-agenda").then((element) => {
+  it("displays date change buttons with allow_date_change prop", () => {
+    cy.get("@agenda").invoke("attr", "allow_date_change", true);
+    cy.get("@agenda").get("button.prev").should("be.visible");
+    cy.get("@agenda").get("button.next").should("be.visible");
+  });
+
+  it("does not display date change buttons without allow_date_change prop", () => {
+    cy.get("@agenda").invoke("attr", "allow_date_change", false);
+    cy.get("@agenda").get("button.prev").should("not.exist");
+    cy.get("@agenda").get("button.next").should("not.exist");
+  });
+
+  it("only allows navigation to dates in allowed_dates prop - allowed_dates as array of Date", () => {
+    cy.get("@agenda").invoke("attr", "allow_date_change", true);
+    cy.get("@agenda").invoke("attr", "selected_date", "");
+    cy.get("@agenda").invoke("attr", "allowed_dates", [
+      new Date("January 14 2022"),
+      new Date("January 29 2022"),
+    ]);
+
+    cy.get("@agenda").contains("Friday 14");
+    cy.get("button.prev").should("have.attr", "disabled");
+    cy.get("button.next").click();
+    cy.get("@agenda").contains("Saturday 29");
+    cy.get("button.next").should("have.attr", "disabled");
+  });
+
+  it("only allows navigation to dates in allowed_dates prop - allowed_dates as comma separated string", () => {
+    cy.get("@agenda").invoke("attr", "allow_date_change", true);
+    cy.get("@agenda").invoke("attr", "selected_date", "");
+    cy.get("@agenda").invoke(
+      "attr",
+      "allowed_dates",
+      "January 14 2022, January 29 2022",
+    );
+
+    cy.get("@agenda").contains("Friday 14");
+    cy.get("button.prev").should("have.attr", "disabled");
+    cy.get("button.next").click();
+    cy.get("@agenda").contains("Saturday 29");
+    cy.get("button.next").should("have.attr", "disabled");
+  });
+
+  it("handles static events in events prop", () => {
+    cy.get("@agenda").then((element) => {
       const agenda = element[0];
       agenda.events = AGENDA_EVENTS;
+    });
+
+    cy.get("@agenda").contains("Static Event 1");
+    cy.get("@agenda").contains(
+      "This event was passed in manually via events prop",
+    );
+  });
+
+  it("passing static events overrides fetched events from calendar ids", () => {
+    cy.get("@agenda").then((element) => {
+      const agenda = element[0];
+      agenda.events = AGENDA_EVENTS;
+    });
+
+    cy.get("@agenda").contains("Static Event 1");
+    cy.get("@agenda").contains(
+      "This event was passed in manually via events prop",
+    );
+    cy.get("@agenda").contains("Sample Meeting 1").should("not.exist");
+    cy.get("@agenda").contains("Sample Meeting 2").should("not.exist");
+    cy.get("@agenda").contains("Sample Meeting 3").should("not.exist");
+    cy.get("@agenda").contains("Sample Meeting 4").should("not.exist");
+  });
+
+  it("removing static events fetches events by calendar ids", () => {
+    cy.get("@agenda").then((element) => {
+      const agenda = element[0];
+      agenda.events = AGENDA_EVENTS;
+    });
+
+    cy.get("@agenda").contains("Static Event 1");
+    cy.get("@agenda").contains(
+      "This event was passed in manually via events prop",
+    );
+
+    cy.get("@agenda").contains("Sample Meeting 1").should("not.exist");
+
+    cy.get("@agenda").then((element) => {
+      const agenda = element[0];
+      agenda.events = null;
+    });
+
+    cy.get("@agenda").contains("Static Event 1").should("not.exist");
+    cy.get("@agenda").contains("Sample Meeting 1");
+    cy.get("@agenda").contains("Sample Meeting 2");
+    cy.get("@agenda").contains("Sample Meeting 3");
+    cy.get("@agenda").contains("Sample Meeting 4");
+  });
+
+  it("24 hour timeline is visible when auto_time_box is false", () => {
+    cy.get("@agenda").then((element) => {
+      const agenda = element[0];
       agenda.auto_time_box = false;
-      // First tick is 12 AM regardless of auto_time_box;
-      cy.get(".ticks").get(".tick:eq(0)").should("contain", "12 AM");
-      // it gets its height offset into the negative if we're timeboxed. Test that.
-      cy.get(".ticks").get(".tick:eq(0)").should("have.css", "top", "0px");
     });
+
+    [
+      "12 AM",
+      "1 AM",
+      "2 AM",
+      "3 AM",
+      "4 AM",
+      "5 AM",
+      "6 AM",
+      "7 AM",
+      "8 AM",
+      "9 AM",
+      "10 AM",
+      "11 AM",
+      "12 PM",
+      "1 PM",
+      "2 PM",
+      "3 PM",
+      "4 PM",
+      "5 PM",
+      "6 PM",
+      "7 PM",
+      "8 PM",
+      "9 PM",
+      "10 PM",
+      "11 PM",
+    ].forEach((time) => cy.get("@agenda").contains(time).should("be.visible"));
   });
-  it("correctly scales view to start and end when events do", () => {
-    cy.visit("/components/agenda/src/index.html");
-    cy.get("nylas-agenda").then((element) => {
+
+  it("shows compacted timeline from first event to last event when auto_time_box is true", () => {
+    cy.get("@agenda").then((element) => {
+      const agenda = element[0];
+      agenda.auto_time_box = true;
+    });
+
+    ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"].forEach(
+      (time) => cy.get("@agenda").contains(time).should("be.visible"),
+    );
+
+    ["9 AM", "6 PM"].forEach((time) =>
+      cy.get("@agenda").contains(time).should("not.be.visible"),
+    );
+  });
+
+  it("compacted timeline adjust when events prop is updated", () => {
+    cy.get("@agenda").then((element) => {
+      const agenda = element[0];
+      agenda.auto_time_box = true;
+    });
+
+    ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"].forEach(
+      (time) => cy.get("@agenda").contains(time).should("be.visible"),
+    );
+
+    ["9 AM", "6 PM"].forEach((time) =>
+      cy.get("@agenda").contains(time).should("not.be.visible"),
+    );
+
+    cy.get("@agenda").then((element) => {
       const agenda = element[0];
       agenda.events = AGENDA_EVENTS;
-      agenda.auto_time_box = true;
-      // first event starts at 10:00am; or 600 minutes into a 1440 minute day.
-      // last event ends at 2:00pm, or 840 minutes into a 1440 minute day
-      // so we're zooming to between 10a-2p, or 600-840, showing 240/1440 minutes.
-      // Our active view therefore shows 4 hours, and are concealing 10 hours before;
-      // 10/4 = 2.5.
-      // We thus expect the "concealed before" viewport to be 250% of our active view,
-      // in otherwords, a top of -250% (or, against our viewport, -1760px)
-      // (viewport height: 800px; .ticks container: 704px. 704 x 2.5 = 1760)
-      cy.get(".ticks").get(".tick:eq(0)").should("have.css", "top", "-1760px");
     });
+
+    ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM"].forEach((time) =>
+      cy.get("@agenda").contains(time).should("be.visible"),
+    );
+
+    ["9 AM", "3 PM", "4 PM", "5 PM", "6 PM"].forEach((time) =>
+      cy.get("@agenda").contains(time).should("not.be.visible"),
+    );
   });
-  it("updates auto_time_box view when events are updated externally", () => {
-    cy.visit("/components/agenda/src/index.html");
-    cy.get("nylas-agenda").then((element) => {
-      const agenda = element[0];
-      agenda.events = AGENDA_EVENTS;
-      agenda.auto_time_box = true;
-      cy.get(".ticks")
-        .get(".tick:eq(0)")
-        .should("have.css", "top", "-1760px")
-        .then(() => {
-          agenda.events = [
-            ...AGENDA_EVENTS.map((slot, i) => {
-              if (i === 0) {
-                slot.when.start_time = 1600444800; // 12:00
-                slot.when.end_time = 1600446600; // 12:30
-              }
-              return slot;
-            }),
-          ];
-          cy.get(".ticks")
-            .get(".tick:eq(0)")
-            .should("have.css", "top", "-4224px");
-        });
-    });
+});
+
+describe("Agenda interactions", () => {
+  beforeEach(() => {
+    cy.clock(1642179039224);
+
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/manifest",
+      {
+        fixture: "agenda/manifest.json",
+      },
+    );
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/calendars/test-calendar-id",
+      {
+        fixture: "agenda/calendars/calendarId.json",
+      },
+    );
+    cy.intercept(
+      "GET",
+      "https://web-components.nylas.com/middleware/agenda/events?calendar_id=test-calendar-id*",
+      {
+        fixture: "agenda/events.json",
+      },
+    );
+
+    cy.visit("/components/agenda/src/cypress.html");
+    cy.get("nylas-agenda").should("exist").as("agenda");
+  });
+
+  it("changes date to tomorrow", () => {
+    cy.get("@agenda").invoke("attr", "allow_date_change", true);
+    cy.get("@agenda").contains("Friday 14");
+    cy.get("button.next").click();
+    cy.get("@agenda").contains("Saturday 15");
+  });
+
+  it("changes date to yesterday", () => {
+    cy.get("@agenda").invoke("attr", "allow_date_change", true);
+    cy.get("@agenda").contains("Friday 14");
+    cy.get("button.prev").click();
+    cy.get("@agenda").contains("Thursday 13");
   });
 });
