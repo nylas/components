8062937e80ffed45d8ab4b43f6a6b43cdca420bc
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2022-01-19 18:47:06 -0500

    Fixed issue where thread expanded in the mailbox component

--- components/mailbox/src/Mailbox.svelte
+++ components/mailbox/src/Mailbox.svelte
@@ -318,8 +318,9 @@
       await dispatchDraft(event);
       return;
     }
-    dispatchEvent("threadClicked", { event, thread });
+
     currentlySelectedThread = thread;
+    thread.expanded = !thread.expanded;
     if (!_this.all_threads && thread?.expanded) {
       if (thread.unread) {
         thread.unread = false;
