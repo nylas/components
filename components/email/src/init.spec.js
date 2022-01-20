03e4409ecd3ec8fccb15d8710af61a886dd9ac60
Pooja Guggari <pooja.g@nylas.com>
2022-01-19 20:43:53 -0500

    test fix and revert

--- components/email/src/init.spec.js
+++ components/email/src/init.spec.js
@@ -937,9 +937,7 @@ describe("Email: Displays threads and messages", () => {
       .find(".snippet")
       .should("exist")
       .and(($div) => {
-        expect($div).to.contain(
-          "Sorry, looks like this thread is currently unavailable",
-        );
+        expect($div).to.contain("This is a draft email");
       });
   });
 });
