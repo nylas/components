52942b47a7b992e2f9533059c7aa901a47d27d6b
Femi Odugbesan <femi.o@nylas.com>
2022-03-07 10:46:39 -0600

    coerce endTime to 23:59 when set to 24:00

--- components/availability/src/method/openhours.ts
+++ components/availability/src/method/openhours.ts
@@ -11,7 +11,7 @@ export function convertHourAssumptionsToOpenHours(
       emails: events.flatMap((event) => event.participantEmails),
       days: [...Array(7).keys()],
       start: `${startHour}:00`,
-      end: `${endHour}:00`,
+      end: coerceEndTime({ hour: endHour }),
       timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
       object_type: "open_hours",
     },
@@ -26,6 +26,27 @@ function formatTime(time: number) {
   return time.toString().padStart(2, "0");
 }
 
+/**
+ * Cloud core expects end time range [0, 23)
+ * So a time range 0:00 - 24:00 (the full day),
+ * which a user might think is a valid range throws an error.
+ *
+ * The actual range should be 0:00 - 23:59.
+ */
+function coerceEndTime({
+  hour,
+  minute = 0,
+}: {
+  hour: number;
+  minute?: number;
+}) {
+  if (hour === 24 && minute === 0) {
+    return `${formatTime(23)}:${formatTime(59)}`;
+  }
+
+  return `${formatTime(hour)}:${formatTime(minute)}`;
+}
+
 export function buildOpenHours(
   openHours: AvailabilityRule[],
   events: EventDefinition[],
@@ -34,7 +55,7 @@ export function buildOpenHours(
     emails: events.flatMap((event) => event.participantEmails),
     days: [rule.startWeekday === 0 ? 6 : rule.startWeekday - 1], // TODO - include endWeekday
     start: `${formatTime(rule.startHour)}:${formatTime(rule.startMinute)}`,
-    end: `${formatTime(rule.endHour)}:${formatTime(rule.endMinute)}`,
+    end: coerceEndTime({ hour: rule.endHour, minute: rule.endMinute }),
     timezone: rule.timeZone ?? Intl.DateTimeFormat().resolvedOptions().timeZone,
     object_type: "open_hours",
   }));
