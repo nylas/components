348ef6aa8c6e9b6fe83c2973c088e29642a96d0f
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2022-06-27 11:56:09 -0400

    reverting changes to isFileAnAttachment

--- commons/src/methods/isFileAnAttachment.ts
+++ commons/src/methods/isFileAnAttachment.ts
@@ -1,15 +1,15 @@
 import type { Message, File } from "@commons/types/Nylas";
 import {
   DisallowedContentTypes,
-  // InlineImageTypes,
+  InlineImageTypes,
 } from "@commons/constants/attachment-content-types";
 
 export const isFileAnAttachment = (message: Message, file: File): boolean =>
   file.content_disposition === "attachment" &&
   file.filename &&
-  // !(
-  //   file.content_id &&
-  //   message.cids?.includes(file.content_id) &&
-  //   InlineImageTypes.includes(file.content_type)
-  // ) &&
+  !(
+    file.content_id &&
+    message.cids?.includes(file.content_id) &&
+    InlineImageTypes.includes(file.content_type)
+  ) &&
   !DisallowedContentTypes.includes(file.content_type);
